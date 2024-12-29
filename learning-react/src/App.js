6bfa765ce2c2682720a36e373345d692b644b487
Prithak koirala <prithakkoirala44@gmail.com>
2024-12-29 20:15:39 +0545

    wya wya wya

---
+++ learning-react/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   <html>
+    <head>
+      <title>Books</title>
+    </head>
+    <body>
+      <h1 className="headingOne">THE books store</h1>
+    </body>
+   </html>
+     
+  );
+}
+
+export default App;
