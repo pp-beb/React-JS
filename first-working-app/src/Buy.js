9b42b555eeae6ba2705753f2c12ba406ffd637e2
Prithak koirala <prithakkoirala44@gmail.com>
2025-01-07 11:46:04 +0545

    wyawyaBuy

--- first-working-app/src/Buy.js
+++ first-working-app/src/Buy.js
@@ -2,7 +2,30 @@ import './Buy.css';
 export function Buy() {
     return(
 <div id="buy">
-    <h1>Buy</h1>
+<h1>Buy</h1>
+
+<section className="BuyDiv">
+<section className="BuyBooks">
+    Pagal Basti
+</section>
+<section className="BuyBooks">
+    Summer love
+</section>
+<section className="BuyBooks">
+    Karnal Blues
+</section>
+</section>
+<section className="BuyDiv2">
+<section className="BuyBooks">
+    Old Man and The Sea
+</section>
+<section className="BuyBooks">
+    Milk and Honey
+</section>
+<section className="BuyBooks">
+    Metamorphisis
+</section>
+</section>
 </div>
     );
 }
